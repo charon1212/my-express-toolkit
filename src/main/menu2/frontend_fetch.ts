3214d5b6a459beed26e5b59279d0d29d3a2ffa29
charon1212 <charons2turquoise@gmail.com>
2022-12-18 23:18:49 +0900

    update.

--- src/main/menu2/frontend_fetch.ts
+++ src/main/menu2/frontend_fetch.ts
@@ -4,31 +4,34 @@ export const frontend_fetch = {
   path: 'frontend/expressToolkit/fetch.ts',
   content: (source: SourceInfo[]) => {
     const defineFunction = ({ name, uri, paramKeys, method, }: SourceInfo) => {
-      const hasRequestBody = method !== 'GET' && method !== 'DELETE';
       const uri2 = uri.split('/').map((v) => v.startsWith(':') ? `\${${v.substring(1)}}` : v).join('/');
-      /** args */
-      const argArray: string[] = [];
-      if (paramKeys.length !== 0) argArray.push('{ ' + paramKeys.join(', ') + ' }: { ' + paramKeys.map((key) => `${key}: string`).join(', ') + ' }');
-      if (hasRequestBody) argArray.push(`body: Request${name}`);
-      const args = argArray.join(', ');
-      /** options */
-      const optionArray: string[] = [];
-      if (method !== 'GET') optionArray.push(`method: '${method}'`);
-      if (hasRequestBody) optionArray.push(`body: JSON.stringify(body)`);
-      const options = optionArray.length === 0 ? '' : `, { ${optionArray.join(', ')} }`;
-      return [
-        `export const fetch${name} = (${args}) =>`,
-        `  fetch(\`\${hostUrl}${uri2}\`${options}).then((res) => res.json() as Promise<Response${name}>)`
-      ];
+      const paramArgs = paramKeys.length === 0 ? `{}` : '{ ' + paramKeys.join(', ') + ' }';
+      const paramType = paramKeys.length === 0 ? `{}` : '{ ' + paramKeys.map((key) => `${key}: string`).join(', ') + ' }';
+      if (method !== 'GET' && method !== 'DELETE') {
+        return `export const fetch${name} = wrapFetchWithBody<${paramType}, Request${name}, Response${name}>('${method}', (${paramArgs}) => \`${uri2}\`)`;
+      } else {
+        return `export const fetch${name} = wrapFetchNoBody<${paramType}, Response${name}>('${method}', (${paramArgs}) => \`${uri2}\`)`;
+      }
     };
 
     const importList = source.flatMap(({ name }) => [`  Request${name},`, `  Response${name},`]);
-    const fetchList = source.flatMap((si) => [si.comment, ...defineFunction(si)]);
+    const fetchList = source.flatMap((si) => [si.comment, defineFunction(si)]);
+
     return `import {
 ${importList.join('\n')}
 } from './type';
 
 const hostUrl = 'http://localhost:3000';
+
+const defaultHeaders = {
+  'content-type': 'application/json'
+};
+const wrapFetchNoBody = <Param, Res>(method: string, uri: (param: Param) => string) =>
+  (param: Param): Promise<Res> =>
+    fetch(\`\${hostUrl}\${uri(param)}\`, { method, headers: { ...defaultHeaders } }).then((res) => res.json());
+const wrapFetchWithBody = <Param, Body, Res>(method: string, uri: (param: Param) => string) =>
+  (param: Param, body: Body): Promise<Res> =>
+    fetch(\`\${hostUrl}\${uri(param)}\`, { method, body: JSON.stringify(body), headers: { ...defaultHeaders } }).then((res) => res.json());
 
 ${fetchList.join('\n')}
 `;
